7b5bf543aac776dd222c2486cf8db6dbe8c11fb8
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-07 11:38:41 +0530

    Add files via upload

--- Function1.c
+++ Function1.c
@@ -4,14 +4,14 @@ int main(void) {
 	int num1, num2, total;
 	int my_input(void);   // prototype declaration
 	void my_welcome(void);
+	int my_addition(int, int);
 	
 	my_welcome();
 	
 	num1 = my_input();
 	num2 = my_input();
 	
-	printf("\nPerforming the addition operation...");
-	total = num1 + num2;
+	total = my_addition(num1, num2);
 	
 	printf("\n\nSo %d + %d = %d...", num1, num2, total);
 	
@@ -23,9 +23,16 @@ void my_welcome(void) {
 int my_input(void) {
 	int number;
 	
-	printf("\n\nPlease enter an integer number: ");
+	printf("\nPlease enter an integer number: ");
 	scanf("%d", &number);
 	
 	return number;
+}
+
+int my_addition(int n1, int n2) {
+	int sum;
+	printf("\nPerforming the addition operation...");
+	sum = n1 + n2;
+	return sum;
 }
 
