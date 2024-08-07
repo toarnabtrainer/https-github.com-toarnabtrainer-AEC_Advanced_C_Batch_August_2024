630c77350f1b117f9b7bcae9821cc2f6725ecb4f
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-07 16:28:38 +0530

    Add files via upload

---
+++ DigitalRoot.c
@@ -0,0 +1,4 @@
+Find out the Digital Root of a user given number n.
+if n = 142 => 1 + 4 + 2 = 7 (Digital Root)
+if n = 119 => 1 + 1 + 9 = 11 => 1 + 1 = 2 (Digital Root)
+if n = 991 => 9 + 9 + 1 = 19 => 1 + 9 = 10 => 1 + 0 = 1 (Digital Root)
