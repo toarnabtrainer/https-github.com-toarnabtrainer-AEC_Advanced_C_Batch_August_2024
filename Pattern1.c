9223d2d6f80dff17c72f112aebcbb4f3044c86bd
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-08 10:24:57 +0530

    Add files via upload

---
+++ Pattern1.c
@@ -0,0 +1,28 @@
+/*
+n = 6 (User input)
+
+               i   .   *
+            ---------------
+.....*         1   5   1
+....***        2   4   3   . => (n - i)
+...*****       3   3   5
+..*******      4   2   7   * => (2 * i - 1)
+.*********     5   1   9
+***********    6   0  11
+            ---------------
+            Tracing Table
+*/
+#include <stdio.h>
+int main(void) {
+	int i,j,n;
+	printf("\n\nPlease enter the number of layers: ");
+	scanf("%d", &n);
+	for(i = 1; i <= n; i++) {
+		for(j = 1; j <= (n - i); j++) printf(".");
+		for(j = 1; j <= (2 * i - 1); j++) printf("*");
+		printf("\n");
+	}
+}
+
+
+
