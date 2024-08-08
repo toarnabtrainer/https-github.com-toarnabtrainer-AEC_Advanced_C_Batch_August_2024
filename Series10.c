cf16646df00c8c1a251b56929d69d8ee7dc5a80f
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-08 15:24:40 +0530

    Add files via upload

---
+++ Series10.c
@@ -0,0 +1,6 @@
+/*
+index -> 1   2   3   4    5   6   7   8   9   10   11  12   
+total =  1 + 2 + 3 + 4 + 10 + 5 + 6 + 7 + 8 + 26 + 9 + 10 + .... n terms
+
+What is the 99-th term of the series? (Do only logical calculations)
+*/
