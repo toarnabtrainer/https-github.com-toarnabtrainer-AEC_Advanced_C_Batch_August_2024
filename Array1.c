d03ac8d86747fd9f31bcac9e0b01fe5472176bc3
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 10:28:27 +0530

    Add files via upload

--- Array1.c
+++ Array1.c
@@ -1,29 +1,34 @@
-
-      0   1   2   3   4
+/*
+n = 5
+       ------ col---------
+       0   1   2   3   4
 --------------------------
- 0 |  1   2   3   4   5
- 1 |  6   7   8   9  10
- 2 | 11  12  13  14  15
- 3 | 16  17  18  19  20
- 4 | 21  22  23  24  25
+| 0 |  1   2   3   4   5   =>  1 + col
+r 1 |  6   7   8   9  10   =>  6 + col
+o 2 | 11  12  13  14  15   => 11 + col => (row * n) + 1 + col
+w 3 | 16  17  18  19  20   => 16 + col
+| 4 | 21  22  23  24  25   => 21 + col
 --------------------------
+*/
 
 #include <stdio.h>
 int main(void) {
-	int arr[5][5], row, col;
-	printf("\n\nFilling up the array with data...");
-	for(row = 0; row < 5; row++) {
-		for(col = 0; col < 5; col++) {
-			arr[row][col] = ???;
+	int arr[10][10], row, col, n = 5;
+	printf("\nPlease enter the dimension of the matrix: ");
+	scanf("%d", &n);
+	
+	printf("\nFilling up the array with data...");
+	for(row = 0; row < n; row++) {
+		for(col = 0; col < n; col++) {
+			arr[row][col] = (row * n) + 1 + col;
 		}
-		printf("\n");
 	}	
-	printf("\n\nDisplaying the content of the array...");
-	for(row = 0; row < 5; row++) {
-		for(col = 0; col < 5; col++) {
+	printf("\n\nDisplaying the content of the array...\n");
+	for(row = 0; row < n; row++) {
+		for(col = 0; col < n; col++) {
 			printf("%4d", arr[row][col]);
 		}
 		printf("\n");
 	}
-	printf("\n\nEnd of the program...")
+	printf("\nEnd of the program...");
 }
