7068468ed21686cff64d6c06f383305387347586
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 16:47:03 +0530

    Add files via upload

--- TriodeNumber.c
+++ TriodeNumber.c
@@ -1,4 +1,4 @@
-
+/*
 Triode Number:
 --------------
 Find out all possble 3-digit Triode Numbers.
@@ -11,7 +11,7 @@ As example:
 	if x = 192, then 2*x = 384 and 3*x = 576, all these three numbers having all distinct digits
 	So x = 192 is a Triode Number.
 	
-for num = 102 to 987
+for num = 102 to 498
 	intialize a[10] with all 0's
 	for times = 1 to 3
 		x = times * num
@@ -25,4 +25,44 @@ for num = 102 to 987
 	end for
 	if successful then print num
 end for
+
+  x => 3
+2*x => 3    4
+3*x => 3,4  4
+*/
+#include <stdio.h>
+int main(void) {
+	int x, num, digit, a[10], times, i;
+	for(num = 102; num <= 498; num++) {
+		for (i = 0; i < 10; i++) a[i] = 0;
+		for (times = 1; times <= 3; times++) {
+			x = times * num;
+			while (x != 0) {
+				digit = x % 10;
+				if (a[digit] != 0) break;
+				else a[digit] = 1;
+				x = x / 10;
+			}
+			if (x != 0) break;
+		}
+		if (x == 0) printf("\n%d, %d and %d...", num, 2*num, 3*num);
+	}
+	printf("\n\nEnd of the program...");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
