1115d87e426941c4b875ae435a11aaf7e35bd4b1
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 16:21:06 +0530

    Add files via upload

--- TriodeNumber.c
+++ TriodeNumber.c
@@ -11,4 +11,17 @@ As example:
 	if x = 192, then 2*x = 384 and 3*x = 576, all these three numbers having all distinct digits
 	So x = 192 is a Triode Number.
 	
+for num = 102 to 987
+	intialize a[10] with all 0's
+	for times = 1 to 3
+		x = times * num
+		while (x != 0)
+			digit = x % 10
+			if (digit in a[10]) then ussuccessful and break
+			digit = int(digit / 10)
+		end while
+		if (unsuccessful) then break
+	end for
+	if successful then print num
+end for
 
