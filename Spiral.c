38b935788cc1a28747d42514cfebf7b145c1e7b4
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 14:09:40 +0530

    Add files via upload

---
+++ Spiral.c
@@ -0,0 +1,18 @@
+
+SPIRAL FILL UP OF A MATRIX (CLOCK-WISE)
+--------------------------------------
+Fill up a matrix with dimension (nxn), where n has been taken from user input,
+with numbers ranging from 1 to n^2, using them once in clock wise spiral manner
+starting from top left corner of the matrix.
+
+As example -
+if n = 5
+      0   1   2   3   4
+--------------------------
+ 0 |  1   2   3   4   5
+ 1 | 16  17  18  19   6
+ 2 | 15  24  25  20   7
+ 3 | 14  23  22  21   8
+ 4 | 13  12  11  10   9
+--------------------------
+
