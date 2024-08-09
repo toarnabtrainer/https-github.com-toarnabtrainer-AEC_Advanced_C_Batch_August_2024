25496fa403f4151106535f2f2e2941ed84673837
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 15:34:56 +0530

    Add files via upload

--- SpiralAnti.c
+++ SpiralAnti.c
@@ -16,6 +16,7 @@ if n = 5                              rr, cc => 0, 1, -1
  4 |  5   6   7   8   9        0   -1                 Pre-Occupied Check
 --------------------------  -----------               1) if (arr[row][col] != 0)
                                1    0
+Spiral-2 output: 1 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 17 24 23 22 21 20 19 18 25
 */
 #include <stdio.h>
 int arr[10][10];
