b66a3de6345a025dd4e21b364cc9ba61f9877e63
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 12:29:35 +0530

    Add files via upload

---
+++ MultiplyBy3.c
@@ -0,0 +1,16 @@
+Find out all possible such numbers which when multiply by 3 then in the product
+the first digit of the source number will come at the last and all other digits
+in the product will maintain the same sequence as they were in the source number.
+
+Here, user will not give any input. When we shall execute our program then such
+numbers will be found and will get printed.
+
+As example:
+	
+	Source number:  xy ... z
+	                     * 3
+	                --------
+	                y ... zx
+	                
+Find out all possible such numbers.
+--------------------------------------------------------------------------------
