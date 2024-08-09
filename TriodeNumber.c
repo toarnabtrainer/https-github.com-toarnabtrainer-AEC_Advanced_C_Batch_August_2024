fca1e273619c786467229fe6742cbb810126aacb
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-09 15:57:57 +0530

    Add files via upload

---
+++ TriodeNumber.c
@@ -0,0 +1,14 @@
+
+Triode Number:
+--------------
+Find out all possble 3-digit Triode Numbers.
+Let x be a Triode Number, then x, 2*x and 3*x will have all distinct digits.
+
+As example:
+	if x = 219, then 2*x = 438 and 3*x = 657, all these three numbers having all distinct digits
+	So x = 219 is a Triode Number.
+	
+	if x = 192, then 2*x = 384 and 3*x = 576, all these three numbers having all distinct digits
+	So x = 192 is a Triode Number.
+	
+
