b8a0d39e327752c6fecd82c779d89d8d8520334e
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-10 15:40:15 +0530

    Add files via upload

--- ArrayPointer1.c
+++ ArrayPointer1.c
@@ -2,7 +2,7 @@
 int main(void) {
 	int arr[10];
 	void array_initialize(int []);
-	void array_display(int *);  // int [], int *
+	void array_display(const int *);  // int [], int *
 	
 	array_initialize(arr);
 	array_display(arr);
@@ -16,8 +16,9 @@ void array_initialize(int arr1[]) {
 		arr1[i] = (i + 1) * 100 + 5;
 	}
 }
-void array_display(int *arr2) {      // int arr2[], int arr2[10000], int arr2[1], int *arr2
+void array_display(const int *arr2) {      // int arr2[], int arr2[10000], int arr2[1], int *arr2
 	int j;
+	// arr2[5] = 99999;
 	printf("\n\nDisplaying the content of the array...");
 	for(j = 0; j < 10; j++) {
 		printf("\nLocation Number %d and content = %d...", j, j[arr2]);  // arr2[j], *(arr2 + j), j[arr2]
