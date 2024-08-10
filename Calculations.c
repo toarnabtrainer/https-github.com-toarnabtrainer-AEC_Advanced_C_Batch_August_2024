11969c54840e658ad0f075e318c5a0b22c97e19f
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-10 15:10:34 +0530

    Add files via upload

--- Calculations.c
+++ Calculations.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 int main(void) {
-	int num1, num2, total, difference, product, quotient;
+	int num1, num2, total, difference, product, quotient, remainder;
 	void my_input(int *);
-	void my_calculations(int, int, int *, int *, int *, int *);
+	void my_calculations(int, int, int *, int *, int *, int *, int *);
 	
 	my_input(&num1);
 	my_input(&num2);
 	printf("\nSo user given inputs are num1 = %d and num2 = %d...", num1, num2);
 	
-	my_calculations(num1, num2, &total, &difference, &product, &quotient);
+	my_calculations(num1, num2, &total, &difference, &product, &quotient, &remainder);
 	printf("\nSo %d + %d = %d...", num1, num2, total);
 	printf("\nSo %d - %d = %d...", num1, num2, difference);
 	printf("\nSo %d * %d = %d...", num1, num2, product);
 	printf("\nSo %d / %d = %d...", num1, num2, quotient);
+	printf("\nSo %d %c %d = %d...", num1, 37, num2, remainder);
 	
 	printf("\n\nEnd of the program...");
 }
-void my_calculations(int n1, int n2, int *tt, int *dd, int *pp, int *qq) {
+void my_calculations(int n1, int n2, int *tt, int *dd, int *pp, int *qq, int *rr) {
 	printf("\nPerforming all the calculations...");
 	*tt = n1 + n2;
 	*dd = n1 - n2;
 	*pp = n1 * n2;
 	*qq = n1 / n2;
+	*rr = n1 % n2;
 }
 void my_input(int *n) {
 	printf("\nPlease enter one integer number: ");
