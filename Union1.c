b2e8b9d1e2c7d077c2ef7149a7f879daa42d870c
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-11 12:52:05 +0530

    Add files via upload

---
+++ Union1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+union record {
+	int int_var;
+	char char_var;
+};
+
+int main(void) {
+	union record rec;
+	
+	printf("\n\nInitializing and displaying union variables...");
+	
+	rec.int_var = 1610;
+	printf("\n\nrec.int_var = %d...", rec.int_var);
+	printf("\nrec.char_var = %d and %c...", rec.char_var, rec.char_var);
+	
+	rec.char_var = 'A';
+	printf("\n\nrec.int_var = %d...", rec.int_var);
+	printf("\nrec.char_var = %d and %c...", rec.char_var, rec.char_var);
+	
+	printf("\n\nEnd of the program...");
+}
+/*
+double dd    => .... .... .... .... .... .... .... .... .... .... .... .... .... .... .... ....
+                --------- --------- --------- --------- --------- --------- --------- --------- 
+                Byte 7    Byte 6    Byte 5    Byte 4    Byte 3    Byte 2    Byte 1    Byte 0
+int ii       =>                                         .... .... .... .... .... .... .... ....
+short int si =>                                                             .... .... .... ....
+char cc      =>                                                                       .... ....
+
+                         616   104  40
+int_var = 1640 = 1024 +  512 + 64 + 32 + 8 = 0000 0000 0000 0000 0000 0110 0110 1000
+                                             --------- --------- --------- ---------
+char_var = 0110 1000 => 104 (h)                                   
+char_var = 'A' = 65 = 64 + 1 =                                             0100 0001
+int_var =>                                   0000 0000 0000 0000 0000 0110 0100 0001
+                                             = 1024+512+65 = 1601
+              586   74   10
+1610 = 1024 + 512 + 64 + 8 + 2 = 0000 0000 0000 0000 0000 0110 0100 1010
+                                 --------- --------- --------- ---------
+                                 (0)       (0)       (6)       (74 -> 'J')
+                                                               0100 0001 => 1024 + 512 + 65 = 1601
+*/ 
+
+
+
+
+
+
+
+
+
+
+
+
