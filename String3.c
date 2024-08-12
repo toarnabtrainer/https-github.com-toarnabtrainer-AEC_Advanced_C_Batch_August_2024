7cd39589992d68dd40097ea56ef0cfde3c7c54da
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-12 11:30:59 +0530

    Add files via upload

--- String3.c
+++ String3.c
@@ -1,6 +1,7 @@
 // string processing
 #include <stdio.h>
 #include <string.h>
+int my_isalpha(char);
 int main(void) {
 	//               0123456789
 	char my_str[] = "unIVERsiTY";
@@ -8,17 +9,11 @@ int main(void) {
 	int my_strlen(const char *);
 	char * my_strupr(char []);
 	char * my_strlwr(char []);
+	char * my_strcaseinvert(char []);
 		
 	printf("\nSo the current string is %s...", my_str);
-	// str_display(my_str);
 	printf("\nLength of the string is %d...", strlen(my_str));
-	printf("\nLength of the string is %d...", my_strlen(my_str));
-	// printf("\nString in the upper case is %s...", strupr(my_str));
-	printf("\nString in the upper case is %s...", my_strupr(my_str));
-	printf("\nSo the current string is %s...", my_str);
-	
-	//printf("\nString in the upper case is %s...", strlwr(my_str));
-	printf("\nString in the upper case is %s...", my_strlwr(my_str));
+    printf("\nAfter case invert the string is %s...", my_strcaseinvert(my_str));
 	printf("\nSo the current string is %s...", my_str);
 	
 	printf("\n\nEnd of the program...");
@@ -73,6 +68,30 @@ char * my_strlwr(char my_str[]) {
 	return my_str;
 }
 
+char * my_strcaseinvert(char my_str[]) {
+	int i = 0;
+	while (my_str[i] != '\x0') {
+		if (my_isalpha(my_str[i])) {
+			my_str[i] ^= 32;
+		}
+		i++;
+	}
+	return my_str;
+}
+// bool True TRUE .T. true
+/*
+'A' => 65 => 64 + 1      => 0100 0001
+                        xor 0010 0000 => 32    ch = ch ^ 32
+                            ---------
+'a' => 97 => 64 + 32 + 1 => 0110 0001
+*/
+int my_isalpha(char ch) {
+	if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
 
 
