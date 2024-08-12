2797d08553726a97d6eacfb3341d7c23bf789575
Dr. Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2024-08-12 11:04:44 +0530

    Add files via upload

--- String2.c
+++ String2.c
@@ -3,25 +3,34 @@
 #include <string.h>
 int main(void) {
 	//               0123456789
-	char my_str[] = "university";
-	void str_display(char []);      // void str_display(char *);
-	int my_strlen(char *);
+	char my_str[] = "unIVERsiTY";
+	void str_display(const char []);      // void str_display(char *);
+	int my_strlen(const char *);
+	char * my_strupr(char []);
+	char * my_strlwr(char []);
 		
 	printf("\nSo the current string is %s...", my_str);
 	// str_display(my_str);
 	printf("\nLength of the string is %d...", strlen(my_str));
 	printf("\nLength of the string is %d...", my_strlen(my_str));
+	// printf("\nString in the upper case is %s...", strupr(my_str));
+	printf("\nString in the upper case is %s...", my_strupr(my_str));
+	printf("\nSo the current string is %s...", my_str);
+	
+	//printf("\nString in the upper case is %s...", strlwr(my_str));
+	printf("\nString in the upper case is %s...", my_strlwr(my_str));
+	printf("\nSo the current string is %s...", my_str);
 	
 	printf("\n\nEnd of the program...");
 }
-int my_strlen(char *my_str) {
+int my_strlen(const char *my_str) {
 	int i = 0;
 	while (my_str[i] != '\x0') {
 		i++;
 	}
 	return i;
 }
-void str_display(char *my_str) {
+void str_display(const char *my_str) {
 	int i = 0;
 	printf("\n\nDisplaying the content of the string...");
 	while (my_str[i] != '\x0') {
@@ -29,7 +38,40 @@ void str_display(char *my_str) {
 		i++;
 	}
 }
-
+/*
+ASCII Codes: (American Standard Code for Information Interchange)
+-----------------------------------------------------------------
+8 bit number: ranging from 0 to 255 (2^8 - 1)
+Can be categorized in two categories:
+Normal ASCII Codes (0 to 127): Printable
+A => 65, B => 66, ..., Z => 90
+a => 97, b => 98, ..., z => 122
+0 => 48, 1 => 49, ..., 9 => 57
+Enter => 13, Tab => 8, Back Space => 9, Space => 32, Esc => 27, NULL => 0
+Extended ASCII Codes (128 to 255): Non-Printable
+*/
+char * my_strupr(char my_str[]) {
+	int i = 0;
+	printf("\n\nDisplaying the content of the string...");
+	while (my_str[i] != '\x0') {
+		if (my_str[i] >= 'a' && my_str[i] <= 'z') {
+			my_str[i] -= 32;
+		}
+		i++;
+	}
+	return my_str;
+}
+char * my_strlwr(char my_str[]) {
+	int i = 0;
+	printf("\n\nDisplaying the content of the string...");
+	while (my_str[i] != '\x0') {
+		
+		???
+		
+		i++;
+	}
+	return my_str;
+}
 
 
 
